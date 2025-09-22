fa675ef8fde73b98fe2b4f4412c1de0adeb186bf
Donald Zou <donaldzou@live.hk>
2025-09-22 13:08:37 +0800

    Update

--- server/api/session.ts
+++ server/api/session.ts
@@ -1,21 +1,32 @@
 import {getQuery} from "#imports";
 import { v4 } from 'uuid'
 import { exec } from "child_process";
+import pool from "~~/utils/pool";
 
 export default defineEventHandler(async (event) => {
     const query = getQuery(event)
-    console.log('hi')
     if (!query.session){
-        const sessionId = v4().toString()
-        const d = await new Promise((resolve, reject) => {
-            exec("ping 1.1.1.1 -c 3", (error, stdout, stderr) => {
-                if (error) {
-                    reject({ error: stderr });
-                } else {
-                    resolve({ output: stdout });
-                }
-            })
-        })
-        console.log(d)
+        const sessionId = v4().toString().replace('-', '')
+        await createSession(sessionId)
+        return {
+            sessionId: sessionId
+        }
+    }else{
+
     }
 })
+
+const createSession = async (sessionId: string): Promise<Boolean> => {
+    const checkSession = await pool.query(
+        'SELECT * FROM sessions WHERE sessionId = $1', [sessionId])
+
+    if (checkSession.rows.length > 0){
+        return false
+    }
+
+    const currentDate = new Date()
+    await pool.query(
+        `INSERT INTO sessions VALUES ($1, $2, null)`, [sessionId, currentDate]
+    )
+    return true
+}
